f409c2890b594ce5ff6066d4d97d6c8dc42ab739
HaroldBarreraNino <hsbarreran@outlook.com>
2023-01-11 13:36:55 -0500

    Navbar

--- pages/components/navbarComponent.js
+++ pages/components/navbarComponent.js
@@ -4,13 +4,13 @@ export default function Navbar() {
 
 
             <div className='container-fluid'>
-                <nav class="navbar navbar-inverse">
-                    <div class="container-fluid">
-                        <ul class="nav navbar-nav">
-                            <li><a id="len1" class="hoverable" href="#">Home</a></li>
-                            <li><a id="len2" class="hoverable" href="#">About</a></li>
-                            <li><a id="len3" class="hoverable" href="#">Portfolio</a></li>
-                            <li><a id="len4" class="hoverable" href="#">Contact</a></li>
+                <nav className="navbar navbar-inverse">
+                    <div className="container-fluid">
+                        <ul className="nav navbar-nav">
+                            <li><a id="len1" className="hoverable" href="#">Home</a></li>
+                            <li><a id="len2" className="hoverable" href="#">About</a></li>
+                            <li><a id="len3" className="hoverable" href="#">Portfolio</a></li>
+                            <li><a id="len4" className="hoverable" href="#">Contact</a></li>
                         </ul>
                     </div>
                 </nav>
